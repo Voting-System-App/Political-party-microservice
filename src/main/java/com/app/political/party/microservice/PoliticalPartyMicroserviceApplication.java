450c293a703db0ea2935857c0f4fbe42494bebdf
Moises Molina <w7moises@gmail.com>
2022-06-10 16:04:37 -0500

    template added

---
+++ src/main/java/com/app/political/party/microservice/PoliticalPartyMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package com.app.political.party.microservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PoliticalPartyMicroserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PoliticalPartyMicroserviceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/app/political/party/microservice/PoliticalPartyMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.political.party.microservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PoliticalPartyMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
