069b88bb8dacb88f924749c83da9dbdf15250447
Moises Molina <w7moises@gmail.com>
2022-06-12 11:52:43 -0500

    validation for adherent added

--- src/main/java/com/app/political/party/microservice/entities/Adherent.java
+++ src/main/java/com/app/political/party/microservice/entities/Adherent.java
@@ -3,13 +3,19 @@ package com.app.political.party.microservice.entities;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+import javax.validation.constraints.NotEmpty;
 
 @Document(collection = "adherent")
 @Data
 public class Adherent {
     @Id
     private String id;
+    @NotEmpty
     private String name;
+    @NotEmpty
+    @Field(name = "last_name")
     private String lastName;
     private Boolean status;
 }
