93dab8d493cf9a18f6449990e3199e5c341e7d38
Moises Molina <w7moises@gmail.com>
2022-07-12 14:18:18 -0500

    candidate service added

---
+++ src/main/java/com/app/political/party/microservice/services/CandidateService.java
@@ -0,0 +1,13 @@
+package com.app.political.party.microservice.services;
+
+import com.app.political.party.microservice.entities.Candidate;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+public interface CandidateService {
+    Flux<Candidate> findAll();
+    Mono<Candidate> findById(String id);
+    Mono<Candidate> save(Candidate candidate);
+    Mono<Candidate> update(Candidate candidate,String id);
+    Mono<Void> delete(String id);
+}
